5a35df303f8ce24375408ea7ee70da52e5edecfb
damiean garciamendez <damieante@gmail.com>
2020-06-18 14:38:14 -0400

    initial Save

---
+++ main.js
@@ -0,0 +1,7 @@
+const parentElement= document.querySelector('.katas-list')
+
+
+//kata1
+const kata1heading=document.createElement('h3')
+parentElement.append(kata1heading)
+kata1heading.append('KATA 1')
